3f6e6332a73d174ef3518845a8318167dc3085cf
Kolte200 <celian.aldehuelo@gmail.com>
2026-04-26 18:54:50 +0200

    feat: Add a configure button

--- ai-editor/src/views/editor.ts
+++ ai-editor/src/views/editor.ts
@@ -7,13 +7,21 @@ export class Editor {
 
   public constructor() {
     this.title_bar = document.getElementById("title_bar_view")!!;
+
     this.editor_canvas = document.getElementById("editor_canvas")!! as HTMLCanvasElement;
     this.ai_graph_editor = new AIGraphEditor(this.editor_canvas);
+
+    const open_graph_settings_btn = document.getElementById("open_graph_settings_btn")!! as HTMLButtonElement;
+    open_graph_settings_btn.addEventListener("click", (_) => this.open_graph_settings());
   }
 
   // public start() {
   //     this.ai_graph_editor.get_editor().start();
   // }
+  //
+  private open_graph_settings() {
+    //
+  }
 
   public close_graph() {
     if (this.ai_graph_editor.get_open_graph() !== null) {
